9bb8d9a9f548179df9795469990eff5021d5ae49
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-12-15 22:07:25 +0300

    throught test

---
+++ test/cfb/throught_test.clj
@@ -0,0 +1,38 @@
+(ns cfb.throught-test
+  (:require [clojure.test :refer :all]
+            [cfb.serializer :refer [make-cfb]]
+            [cfb.parser :refer [open-cfb open-stream read-stream]]
+            [cfb.constants :refer [SectorSize]]))
+
+(deftest test-throught
+  (let [data (byte-array (concat
+                          (byte-array SectorSize (byte \A))
+                          (byte-array SectorSize (byte \B))
+                          (byte-array SectorSize (byte \C))))]
+
+    (make-cfb "test.cfb" [["stream" data]])
+
+    (testing "full"
+      (let [cfb (open-cfb "test.cfb")
+            stream (open-stream cfb "stream")
+            result (read-stream stream)]
+        (is (java.util.Arrays/equals data result))))
+
+    (testing "length"
+      (let [cfb (open-cfb "test.cfb")
+            stream (open-stream cfb "stream")
+            result (read-stream stream (+ SectorSize (/ SectorSize 2)))
+            expected (byte-array (concat
+                                  (byte-array SectorSize (byte \A))
+                                  (byte-array (/ SectorSize 2) (byte \B))))]
+        (is (java.util.Arrays/equals result expected))))
+
+    (testing "offset"
+      (let [cfb (open-cfb "test.cfb")
+            stream (open-stream cfb "stream")
+            result (read-stream stream (+ SectorSize (/ SectorSize 2))
+                                (+ SectorSize (/ SectorSize 2)))
+            expected (byte-array (concat
+                                  (byte-array (/ SectorSize 2) (byte \B))
+                                  (byte-array SectorSize (byte \C))))]
+        (is (java.util.Arrays/equals result expected))))))
