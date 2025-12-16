a600aa4f2e287ed6e28ff005780b67bdd7b179a3
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-12-16 20:28:47 +0300

    refactoring tests

--- test/cfb/throught_test.clj
+++ test/cfb/throught_test.clj
@@ -4,33 +4,28 @@
             [cfb.parser :refer [open-cfb open-stream read-stream]]
             [cfb.constants :refer [SectorSize]]))
 
-(deftest test-throught
+(deftest test-read-stream
   (let [data (byte-array (concat
                           (byte-array SectorSize (byte \A))
                           (byte-array SectorSize (byte \B))
-                          (byte-array SectorSize (byte \C))))]
-
-    (make-cfb "test.cfb" [["stream" data]])
+                          (byte-array SectorSize (byte \C))))
+        _  (make-cfb "test.cfb" [["stream" data]])
+        cfb (open-cfb "test.cfb")
+        stream (open-stream cfb "stream")]
 
     (testing "full"
-      (let [cfb (open-cfb "test.cfb")
-            stream (open-stream cfb "stream")
-            result (read-stream stream)]
+      (let [result (read-stream stream)]
         (is (java.util.Arrays/equals data result))))
 
     (testing "length"
-      (let [cfb (open-cfb "test.cfb")
-            stream (open-stream cfb "stream")
-            result (read-stream stream (+ SectorSize (/ SectorSize 2)))
+      (let [result (read-stream stream (+ SectorSize (/ SectorSize 2)))
             expected (byte-array (concat
                                   (byte-array SectorSize (byte \A))
                                   (byte-array (/ SectorSize 2) (byte \B))))]
         (is (java.util.Arrays/equals result expected))))
 
     (testing "offset"
-      (let [cfb (open-cfb "test.cfb")
-            stream (open-stream cfb "stream")
-            result (read-stream stream (+ SectorSize (/ SectorSize 2))
+      (let [result (read-stream stream (+ SectorSize (/ SectorSize 2))
                                 (+ SectorSize (/ SectorSize 2)))
             expected (byte-array (concat
                                   (byte-array (/ SectorSize 2) (byte \B))
